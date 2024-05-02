82ffe710c35c0add0ec62bce4dc426c32f91b189
joshua-gatto <joshuagatto@cmail.carleton.ca>
2024-05-02 14:51:09 -0400

    Added base website code

---
+++ assets/nav_bar.php
@@ -0,0 +1,27 @@
+<?php
+Echo 
+'
+<div class="navBar">
+<a href="#">
+    <div id="currentPage">
+        <p>Home</p>
+    </div>
+</a>
+<a href="portfolio_directory.html">
+    <div>
+        <p>Portfolio</p>
+    </div>
+</a>
+<a href="resume.html">
+    <div>
+        <p>Resume</p>
+    </div>
+</a>
+<a href="about.html">
+    <div>
+        <p>About</p>
+    </div>
+</a>
+</div>
+'
+?>
