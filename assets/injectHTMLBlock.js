b4af2e00713e661dca7d352a5d19bdebaf40b2c5
joshua-gatto <joshuagatto@cmail.carleton.ca>
2024-05-03 16:21:52 -0400

    Added feature to specify target element for injected code blocks

--- assets/injectHTMLBlock.js
+++ assets/injectHTMLBlock.js
@@ -1,4 +1,4 @@
-function loadHTMLBlock(url, callback) {
+function loadHTMLBlock(url, callback, targetId) {
     // Async. fetches HTML file
     var xhr = new XMLHttpRequest();
     // initialize request
@@ -7,19 +7,25 @@ function loadHTMLBlock(url, callback) {
     xhr.onreadystatechange = function() {
         // verifying event
         if(xhr.readyState == 4 && xhr.status == 200) {
-            callback(xhr.responseText);
+            callback(xhr.responseText, targetId);
         }
     };
     // send the request
     xhr.send();
 }
 
-function insertHTMLBlock(html) {
-    // get parent of script tag
-    var scriptTag = document.currentScript || (function() {
-        var scripts = document.getElementsByTagName('script');
-        return scripts[scripts.length - 1];
-    })();
-    var parent = scriptTag.parentElement;
-    parent.innerHTML = html;
+function insertHTMLBlock(html, targetId) {
+    // if target is specified
+    if(targetId == null) {
+        // get script tags
+        var scriptTag = document.currentScript || (function() {
+            var scripts = document.getElementsByTagName('script');
+            return scripts[scripts.length - 1];
+        })();
+        var target = scriptTag.parentElement;
+    } else {
+        // get target
+        var target = document.getElementById(targetId);
+    }
+    target.innerHTML = html;
 }
