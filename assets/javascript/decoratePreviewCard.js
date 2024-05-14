3cf973ce29374f405353f94344f57df580acfd48
joshua-gatto <joshuagatto@cmail.carleton.ca>
2024-05-14 16:47:18 -0400

    refined the style of the page

--- assets/javascript/decoratePreviewCard.js
+++ assets/javascript/decoratePreviewCard.js
@@ -1,5 +1,5 @@
 function decoratePreviewCard(previewCard, ...args){
 	var [imageResource, imageAlt, titleText, descriptionText] = args.flat(2);
-	previewCard = previewCard.replace(/src="[^"]*"/, `src="../resources/${imageResource}"`).replace(/alt="[^"]*"/, `alt="${imageAlt}"`).replace(/<h2>[^<]*<\/h2>/, `<h2>${titleText}</h2>`).replace(/<p>[^<]*<\/p>/, `<p>${descriptionText}</p2>`);
+	previewCard = previewCard.replace(/src="[^"]*"/, `src="../resources/${imageResource}"`).replace(/alt="[^"]*"/, `alt="${imageAlt}"`).replace(/<h2>[^<]*<\/h2>/, `<h2>${titleText}</h2>`).replace(/<p>[^<]*<\/p>/, `<p>${descriptionText}</p>`);
 	return previewCard;
 }
