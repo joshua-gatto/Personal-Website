b1f4b23b3f5a50af4a72059f9f25e5718c31ccdb
joshua-gatto <joshuagatto@cmail.carleton.ca>
2024-05-23 13:07:30 -0400

    changed element injector to use query selectors

--- assets/javascript/injectFormattedElement.js
+++ assets/javascript/injectFormattedElement.js
@@ -1,7 +1,30 @@
 //file needs to be renamed. possibly inject local element
-function injectElement(htmlResource, preprocessing, targetTag, ...preprocessArgs){
-    window.addEventListener('DOMContentLoaded', function() {
+async function injectElement(htmlResource, targetTag, postProcessing, ...postProcessArgs){
+    window.addEventListener('DOMContentLoaded', async function() {
+		const resourceURL = `http://localhost:8000/resources/html/${htmlResource}.html`;
+		try{
+			//fetch resources
+			const response = await fetch(resourceURL);
+			const htmlContent = await response.text();
+			//instantite resource
+			var element = new DOMParser().parseFromString(htmlContent, 'text/html');
+			//get target
+			const targetElement = document.querySelector(targetTag);
+			//decorate
+			if(postProcessing){
+				if(Array.isArray(postProcessing)){
+					postProcessing.forEach((func, index) => {
+						element = func(element, postProcessArgs[index] ? postProcessArgs[index] : null);
+					});
+				}else{
+					element = postProcessing(element, postProcessArgs? postProcessArgs : null);
+				}
+			}
+			targetElement.innerHTML = element.documentElement.innerHTML;
+		} catch(error){
+			console.log(error);
+		}
         // TODO: remove/replace http://localhost:8080 when deploying
-        loadHTMLBlock(`http://localhost:8000/resources/html/${htmlResource}.html`, injectHTMLBlock, preprocessing, targetTag? targetTag : null, preprocessArgs? preprocessArgs : null);
+        // loadHTMLBlock(`http://localhost:8000/resources/html/${htmlResource}.html`, injectHTMLBlock, preprocessing, targetTag? targetTag : null, preprocessArgs? preprocessArgs : null);
     });
 }
