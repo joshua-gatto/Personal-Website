5f3871f8fb7850c0b98ffc96db0a2268cc91aebc
joshua-gatto <joshuagatto@cmail.carleton.ca>
2024-06-05 00:09:22 -0400

    created method to inject content and formatting from json into project pages

--- assets/js/decorate/decorateSubjectCard.js
+++ assets/js/decorate/decorateSubjectCard.js
@@ -1,21 +1,28 @@
 function decorateSubjectCard(subjectCard, ...args) {
     const subject = args.flat()[0];
-    const contentElement = buildContent(subject);
+	const h2 = subjectCard.querySelector("h2");
+	h2.textContent = subject.h2;
+	console.log(subject.content);
+    const contentElement = buildContent(subject.content);
     subjectCard.appendChild(contentElement);
     return subjectCard;
 }
 
-function buildContent(element) {
+function buildContent(elements) {
     try {
-        const createdElement = document.createElement(element.tag);
-        Object.entries(element).forEach(([key, value]) => {
-            if (key !== "tag" && key !== "text") {
-                createdElement.setAttribute(key, value);
-            } else if (key === "text") {
-                createdElement.textContent = value;
-            }
+        const fragment = document.createDocumentFragment();
+        elements.forEach(element => {
+            const createdElement = document.createElement(element.tag);
+            Object.entries(element).forEach(([key, value]) => {
+                if (key !== "tag" && key !== "text") {
+                    createdElement.setAttribute(key, value);
+                } else if (key === "text") {
+                    createdElement.textContent = value;
+                }
+            });
+            fragment.appendChild(createdElement);
         });
-        return createdElement;
+        return fragment;
     } catch (error) {
         console.log("Error compiling subjectCard content ", error);
         return null;
